b0d55b4bdc519d3951bb3de855bb28bc05f52140
michellegutierrez <gut20014@byui.edu>
2024-03-02 12:37:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import './styles.css'
+import { NewToDoForm } from '../NewTodoForm'
+import { TodoList } from './TodoList'
+
+export default function App(){
+
+   const [todos , setTodos] = useState(() =>{
+    const localValue = localStorage.getItem("items")
+    if(localValue == null) return []
+
+    return JSON.parse(localValue)
+   })
+
+
+   useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return  currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+  return  (
+  <>
+   <NewToDoForm onSubmit={addTodo} />
+  <h1 className='header'>To  Do List</h1>
+ <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+  </>
+  )
+}
